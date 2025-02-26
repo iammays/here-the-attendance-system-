119fc41440fc3ce3fab12dd5517776ba26384565
iammays <maysalbutmah311@gmail.com>
2025-02-26 14:15:42 +0200

    all AI done work 95%

--- ai/test_ai.py
+++ ai/test_ai.py
@@ -1,4 +1,15 @@
-from yolo_detector import process_video_stream
+from recognition import process_video_stream
 
-video_path = "3.mp4"  # اسم الفيديو
-process_video_stream(video_path)
+
+video_path = "8.mp4" 
+#process_video_stream(video_path)
+
+
+
+
+from main import recognize_faces_from_video 
+
+
+# video_path = "2.mp4"  
+
+recognize_faces_from_video(video_path)
