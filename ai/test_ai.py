38cd4f708f8b25b43a056c180f22e4789272e0c0
Joelle <joellematar14@gmail.com>
2025-03-12 09:24:38 +0200

    merged mays branch to joelle branch

--- ai/test_ai.py
+++ ai/test_ai.py
@@ -1,4 +1,15 @@
-from recognition import recognize_faces_from_video
+from recognition import process_video_stream
 
-video_path = "video.mp4"  # اسم الفيديو
+
+video_path = "8.mp4" 
+#process_video_stream(video_path)
+
+
+
+
+from main import recognize_faces_from_video 
+
+
+# video_path = "2.mp4"  
+
 recognize_faces_from_video(video_path)
