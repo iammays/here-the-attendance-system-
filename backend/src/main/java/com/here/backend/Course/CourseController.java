75f5a464368d204aa88368c6f81cfd0ae280c57a
shouq <shouqalmaharbah@gmail.com>
2025-03-17 15:15:08 +0200

    security pro

--- backend/src/main/java/com/here/backend/Course/CourseController.java
+++ backend/src/main/java/com/here/backend/Course/CourseController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
